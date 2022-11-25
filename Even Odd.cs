99332f9127df40ed6cf1dde9dc97c84bff1ffb65
Yukthasam06 <113716190+Yukthasam06@users.noreply.github.com>
2022-11-25 16:34:01 +0530

    Create Even Odd.cs

---
+++ Even Odd.cs
@@ -0,0 +1,15 @@
+using System;
+
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        int a=Convert.ToInt32(Console.ReadLine());
+        if(a%2==0){
+            Console.WriteLine ("{0} is even",a);
+        }
+        else{
+            Console.WriteLine ("{0} is odd",a);   
+        }
+    }
+}
